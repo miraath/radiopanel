15d6f55b47fbdf81795d66d78d9136ad1dfa8631
Mohamed <mohamed.rez@gmail.com>
2017-02-25 23:11:54 +0000

    only authenticated users can add radiostations

--- app/controllers/radiostations_controller.rb
+++ app/controllers/radiostations_controller.rb
@@ -3,7 +3,7 @@ require 'net/http'
 
 class RadiostationsController < ApplicationController
   before_action :set_radiostation, only: [:show, :edit, :update, :destroy]
-  # before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @radiostations = Radiostation.where(active: true)
